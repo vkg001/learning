89c60e7001fbde9507a7f9efe57b961e62dadd7d
Vikas Kumar <vkg360.vikas@gmail.com>
2021-09-17 19:03:20 +0530

    diff midified

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diff {
     public static void main(String args[]) {
         int c, d;
         c = 10 ;    d = 15; 
-        System.out.println("Diff : " + (d-c));
+        System.out.println("Difference of 15 and 10 is  " + (d-c));
     }
 }
