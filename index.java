8a5257faa7e908e88de236a544ace8d81252c481
Vikas Kumar <vkg360.vikas@gmail.com>
2021-09-17 19:43:13 +0530

    index

--- index.java
+++ index.java
@@ -2,6 +2,6 @@ class index {
     public static void main(String args[]) {
         int a, b;
         a = 5;  b = 10;
-        System.out.println("Sum : " + a+b);
+        System.out.println("Sum of 5 and 10 is " + a+b);
     }
 }
