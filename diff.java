9fa12df9b1735940133f57a2e23d95105ff8476b
Vikas Kumar <vkg360.vikas@gmail.com>
2021-09-17 18:58:02 +0530

    add all

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class diff {
+    public static void main(String args[]) {
+        int c, d;
+        c = 10 ;    d = 15; 
+        System.out.println("Diff : " + (d-c));
+    }
+}
---
+++ index.java
@@ -0,0 +1,7 @@
+class index {
+    public static void main(String args[]) {
+        int a, b;
+        a = 5;  b = 10;
+        System.out.println("Sum of 5 & 10 " + a+b);
+    }
+}
