1da0d7c68cec19ca899512cccd5e6c8d2b0c8cb5
Vikas Kumar <vkg360.vikas@gmail.com>
2021-09-17 19:12:15 +0530

    multiply added

---
+++ multiply.java
@@ -0,0 +1,7 @@
+public class multiply {
+    public static void main(String args[]) {
+        int x, y;
+        x = 5;  y = 10 ;
+        System.out.println("Multiplication of 5 and 10 is " + (x*y));
+    }
+}
